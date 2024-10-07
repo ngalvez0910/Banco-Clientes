131f8f75fb4a59f26abbcf846dd8cdb844363113
TomasVaquerin <vaquerinjimenez@gmail.com>
2024-10-07 22:48:25 +0200

    add the cache and its implementation

--- Banco_Clientes_Grupal/src/main/java/org/example/clientes/repositories/TarjetaRemoteRepository.java
+++ Banco_Clientes_Grupal/src/main/java/org/example/clientes/repositories/TarjetaRemoteRepository.java
@@ -1,14 +1,8 @@
 package org.example.clientes.repositories;
 
-import io.vavr.control.Either;
 import org.example.clientes.model.Tarjeta;
+import org.example.common.Repository;
 
 import java.util.List;
 
-public interface TarjetaRemoteRepository {
-    public Either<String, List<Tarjeta>> getAll();
-    public Either<String, Tarjeta> getById(Long id);
-    public Either<String, Tarjeta> create(Tarjeta tarjeta);
-    public Either<String, Tarjeta> update(Long id, Tarjeta tarjeta);
-    public Either<String, Boolean> delete(Long id);
-}
+public interface TarjetaRemoteRepository extends Repository<Tarjeta, Long> {}
