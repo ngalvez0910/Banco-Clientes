012d0288cb5c58f44ded68cb33049ec4448910b0
Natalia González Álvarez <nagonal2004@gmail.com>
2024-10-09 20:53:02 +0200

    tests remote repo

--- Banco_Clientes_Grupal/src/test/resources/init.sql
+++ Banco_Clientes_Grupal/src/test/resources/init.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS tarjetas (
     id SERIAL PRIMARY KEY,
     nombreTitular VARCHAR(255) NOT NULL,
     numeroTarjeta VARCHAR(255) NOT NULL,
-    fechaCaducidad VARCHAR(5) NOT NULL
+    fechaCaducidad TIMESTAMP NOT NULL
 );
 
 INSERT INTO tarjetas (id, nombreTitular, numeroTarjeta, fechaCaducidad)
-VALUES (1,'Ana', '1234567890123456', '01/25'),
-       (2,'Maria', '2468101213141516', '06/28');
+VALUES (1,'Ana', '1234567890123456', '2025-01-01 00:00:00'),
+       (2,'Maria', '2468101213141516', '2028-06-01 00:00:00');
