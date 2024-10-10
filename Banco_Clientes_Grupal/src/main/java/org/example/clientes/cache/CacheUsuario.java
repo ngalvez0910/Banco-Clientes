eadfa8aefcffe26c78c7663938975dfb4e2956f9
TomasVaquerin <vaquerinjimenez@gmail.com>
2024-10-10 17:45:57 +0200

    Cache Cliente

--- Banco_Clientes_Grupal/src/main/java/org/example/clientes/cache/CacheUsuario.java
+++
@@ -1,8 +0,0 @@
-package org.example.clientes.cache;
-
-import org.example.clientes.model.Usuario;
-import org.example.common.Cache;
-
-public interface CacheUsuario extends Cache<Long, Usuario> {
-    int USUARIO_CACHE_SIZE = 5;
-}
--- Banco_Clientes_Grupal/src/main/java/org/example/clientes/cache/CacheUsuarioImpl.java
+++
@@ -1,9 +0,0 @@
-package org.example.clientes.cache;
-
-import org.example.clientes.model.Usuario;
-
-public class CacheUsuarioImpl extends CacheImpl<Long, Usuario> implements CacheUsuario{
-    public CacheUsuarioImpl(int cacheSize) {
-        super(cacheSize);
-    }
-}
