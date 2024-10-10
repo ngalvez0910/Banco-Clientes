625093e5098effad4ba46dc274a21e6d5286b5bd
Natalia González Álvarez <nagonal2004@gmail.com>
2024-10-10 12:59:33 +0200

    cambios en getAll

--- Banco_Clientes_Grupal/src/main/resources/init.sql
+++ Banco_Clientes_Grupal/src/main/resources/init.sql
@@ -1,8 +1,6 @@
--- Eliminar las tablas si existen
 DROP TABLE IF EXISTS Tarjeta;
 DROP TABLE IF EXISTS Usuario;
 
--- Crear la tabla Usuario
 CREATE TABLE IF NOT EXISTS Usuario (
     id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(16)))),
     nombre VARCHAR(255) NOT NULL,
@@ -10,9 +8,8 @@ CREATE TABLE IF NOT EXISTS Usuario (
     email VARCHAR(255) NOT NULL UNIQUE,
     createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
+);
 
--- Crear la tabla Tarjeta
 CREATE TABLE IF NOT EXISTS Tarjeta (
     id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(16)))),
     numeroTarjeta VARCHAR(16) NOT NULL,
@@ -21,4 +18,4 @@ CREATE TABLE IF NOT EXISTS Tarjeta (
     createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (clientID) REFERENCES Cliente(id) ON DELETE CASCADE
-    );
+);
