e924becce48d744ff934cf587fb318c44f27c302
Natalia González Álvarez <nagonal2004@gmail.com>
2024-10-11 16:09:53 +0200

    cacheTests

--- Banco_Clientes_Grupal/src/test/java/org/example/clientes/cache/CacheImplTest.java
+++ Banco_Clientes_Grupal/src/test/java/org/example/clientes/cache/CacheImplTest.java
@@ -1,264 +1,178 @@
 package org.example.clientes.cache;
 
+import org.example.clientes.model.Cliente;
 import org.example.clientes.model.Tarjeta;
+import org.example.clientes.model.Usuario;
 import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
-import java.util.Collection;
-
+import java.time.LocalDateTime;
+import java.util.Collections;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CacheImplTest {
 
-    private Tarjeta Tarjeta1 () {
-        return Tarjeta.builder()
+    private Cliente Cliente1() {
+        return Cliente.builder()
                 .id(1L)
-                .nombreTitular("Juan Pérez")
-                .numeroTarjeta("1234 5678 9012 3456")
-                .fechaCaducidad(LocalDate.of(2025, 12, 31))
-                .build();
-    }
-    private Tarjeta Tarjeta2 () {
-        return Tarjeta.builder()
-                .id(2L)
-                .nombreTitular("Juan Pérez")
-                .numeroTarjeta("1234 5678 9012 3456")
-                .fechaCaducidad(LocalDate.of(2025, 12, 31))
-                .build();
-    }
-    private Tarjeta Tarjeta3 () {
-        return Tarjeta.builder()
-                .id(3L)
-                .nombreTitular("Juan Pérez")
-                .numeroTarjeta("1234 5678 9012 3456")
-                .fechaCaducidad(LocalDate.of(2025, 12, 31))
+                .usuario(Usuario.builder()
+                        .id(1L)
+                        .nombre("Juan")
+                        .userName("juan123")
+                        .email("juan.perez@example.com")
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build())
+                .tarjeta(Collections.singletonList(Tarjeta.builder()
+                        .id(1L)
+                        .nombreTitular("Juan")
+                        .numeroTarjeta("1234-5678-9012-3456")
+                        .fechaCaducidad(LocalDate.of(2025, 5, 31))
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build()))
                 .build();
     }
 
-    private Tarjeta Tarjeta4 () {
-        return Tarjeta.builder()
-                .id(4L)
-                .nombreTitular("Juan Pérez")
-                .numeroTarjeta("1234 5678 9012 3456")
-                .fechaCaducidad(LocalDate.of(2025, 12, 31))
+    private Cliente Cliente2() {
+        return Cliente.builder()
+                .id(2L)
+                .usuario(Usuario.builder()
+                        .id(2L)
+                        .nombre("Pedro")
+                        .userName("pedro456")
+                        .email("pedro.sanchez@example.com")
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build())
+                .tarjeta(Collections.singletonList(Tarjeta.builder()
+                        .id(2L)
+                        .nombreTitular("Pedro")
+                        .numeroTarjeta("9876-5432-1098-7654")
+                        .fechaCaducidad(LocalDate.of(2025, 7, 31))
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build()))
                 .build();
     }
 
     @Test
     void get() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(2);
-        Tarjeta tarjeta1 = Tarjeta1();
+        CacheImpl cache = new CacheImpl(2);
+        Cliente cliente1 = Cliente1();
 
-        cache.put(tarjeta1.getId(), tarjeta1);
+        cache.put(cliente1.getId(), cliente1);
 
-        assertAll(
-                "Verificar que la tarjeta se guarda y se puede recuperar correctamente",
-                () -> assertNotNull(cache.get(tarjeta1.getId()),
-                        "La tarjeta debería estar presente en la caché"),
-                () -> assertEquals(tarjeta1, cache.get(tarjeta1.getId()),
-                        "La tarjeta recuperada debería ser igual a la tarjeta almacenada")
-        );
+        assertAll( () -> {
+            assertNotNull(cache.get(cliente1.getId()), "El cliente debería estar presente en la caché");
+            assertEquals(cliente1, cache.get(cliente1.getId()), "El cliente recuperado debería ser igual al almacenado");
+        });
     }
 
     @Test
     void getNotFound() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(2);
-        Long idNoExistente = 4L;
+        CacheImpl cache = new CacheImpl(2);
+        Long idNoExistente = 3L;
 
-        assertAll(
-                "Verificar que la tarjeta no está presente en la caché",
-                () -> assertNull(cache.get(idNoExistente),
-                        "La tarjeta no debería estar presente en la caché y debería devolver null")
-        );
+        assertAll( () -> {
+            assertNull(cache.get(idNoExistente), "El cliente no debería estar presente en la caché y debería devolver null");
+        });
     }
 
     @Test
     void put() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(2);
-        Tarjeta tarjeta1 = Tarjeta1();
-        Tarjeta tarjeta2 = Tarjeta2();
+        CacheImpl cache = new CacheImpl(2);
+        Cliente cliente1 = Cliente1();
+        Cliente cliente2 = Cliente2();
 
-        cache.put(tarjeta1.getId(), tarjeta1);
-        cache.put(tarjeta2.getId(), tarjeta2);
+        cache.put(cliente1.getId(), cliente1);
+        cache.put(cliente2.getId(), cliente2);
 
-        assertAll(
-                "Verificar que las tarjetas se guardan y se pueden recuperar",
-                () -> assertEquals(2, cache.size(), "La caché debería contener dos elementos"),
-                () -> assertEquals(tarjeta1, cache.get(tarjeta1.getId()),
-                        "La tarjeta1 recuperada debería ser igual a la tarjeta1 almacenada"),
-                () -> assertEquals(tarjeta2, cache.get(tarjeta2.getId()),
-                        "La tarjeta2 recuperada debería ser igual a la tarjeta2 almacenada")
-        );
+        assertAll( () -> {
+            assertEquals(2, cache.size(), "La caché debería contener dos clientes");
+            assertEquals(cliente1, cache.get(cliente1.getId()), "El cliente1 recuperado debería ser igual al cliente1 almacenado");
+            assertEquals(cliente2, cache.get(cliente2.getId()), "El cliente2 recuperado debería ser igual al cliente2 almacenado");
+        });
     }
 
     @Test
     void putOverrider() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(2);
-        Tarjeta tarjeta1 =  Tarjeta1();
-        Tarjeta tarjeta2 =  Tarjeta2();
-        Tarjeta tarjeta3 = Tarjeta3();
-        Tarjeta tarjeta4 = Tarjeta4();
+        CacheImpl cache = new CacheImpl(2);
+        Cliente cliente1 = Cliente1();
+        Cliente cliente2 = Cliente2();
+        Cliente cliente3 = Cliente.builder().id(3L).build();
 
-        cache.put(tarjeta1.getId(), tarjeta1);
-        cache.put(tarjeta2.getId(), tarjeta2);
-        cache.put(tarjeta1.getId(), tarjeta1);
-        cache.put(tarjeta3.getId(), tarjeta3);
-        cache.put(tarjeta4.getId(), tarjeta4);
-        cache.put(tarjeta1.getId(), tarjeta1);
+        cache.put(cliente1.getId(), cliente1);
+        cache.put(cliente2.getId(), cliente2);
+        cache.put(cliente3.getId(), cliente3);
 
-        assertAll(
-                "Verificar que la caché elimina el elemento menos usado cuando se supera el límite",
-                () -> assertEquals(2, cache.size(), "La caché debería tener dos elementos"),
-                () -> assertFalse(cache.containsKey(tarjeta2.getId()), "La caché no debería contener tarjeta2"),
-                () -> assertFalse(cache.containsKey(tarjeta3.getId()), "La caché no debería contener tarjeta3"),
-                () -> assertEquals(tarjeta1, cache.get(tarjeta1.getId()),
-                        "La tarjeta1 debería estar aún en la caché"),
-                () -> assertEquals(tarjeta4, cache.get(tarjeta4.getId()),
-                        "La tarjeta4 debería estar en la caché")
-        );
-    }
-
-    @Test
-    void containsKey() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(2);
-        Tarjeta tarjeta1 = Tarjeta1();
-        Tarjeta tarjeta2 = Tarjeta2();
-
-        cache.put(tarjeta1.getId(), tarjeta1);
-        cache.put(tarjeta2.getId(), tarjeta2);
-
-        cache.clear();
-
-        assertAll(
-                "Verificar que la caché se vacía correctamente",
-                () -> assertEquals(0, cache.size(), "La caché debería estar vacía"),
-                () -> assertFalse(cache.containsKey(tarjeta1.getId()),
-                        "La caché no debería contener tarjeta1"),
-                () -> assertFalse(cache.containsKey(tarjeta2.getId()),
-                        "La caché no debería contener tarjeta2")
-        );
+        assertAll( () -> {
+            assertEquals(2, cache.size(), "La caché debería contener dos clientes");
+            assertFalse(cache.containsKey(cliente1.getId()), "La caché no debería contener cliente1");
+            assertTrue(cache.containsKey(cliente3.getId()), "La caché debería contener cliente3");
+            assertTrue(cache.containsKey(cliente2.getId()), "La caché debería contener cliente2");
+        });
     }
 
     @Test
     void remove() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(3);
-        Tarjeta tarjeta1 = Tarjeta1();
-        Tarjeta tarjeta2 = Tarjeta2();
+        CacheImpl cache = new CacheImpl(3);
+        Cliente cliente1 = Cliente1();
+        Cliente cliente2 = Cliente2();
 
-        cache.put(tarjeta1.getId(), tarjeta1);
-        cache.put(tarjeta2.getId(), tarjeta2);
+        cache.put(cliente1.getId(), cliente1);
+        cache.put(cliente2.getId(), cliente2);
 
-        cache.remove(tarjeta1.getId());
+        cache.remove(cliente1.getId());
 
-        assertAll(
-                "Verificar que tarjeta1 ha sido eliminada y tarjeta2 permanece",
-                () -> assertFalse(cache.containsKey(tarjeta1.getId()), "La caché no debería contener tarjeta1"),
-                () -> assertTrue(cache.containsKey(tarjeta2.getId()), "La caché debería contener tarjeta2")
-        );
+        assertAll( () -> {
+            assertFalse(cache.containsKey(cliente1.getId()), "La caché no debería contener cliente1");
+            assertTrue(cache.containsKey(cliente2.getId()), "La caché debería contener cliente2");
+        });
     }
 
     @Test
     void clear() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(2);
-        Tarjeta tarjeta1 = Tarjeta1();
-        Tarjeta tarjeta2 = Tarjeta2();
+        CacheImpl cache = new CacheImpl(2);
+        Cliente cliente1 = Cliente1();
+        Cliente cliente2 = Cliente2();
 
-        cache.put(tarjeta1.getId(), tarjeta1);
-        cache.put(tarjeta2.getId(), tarjeta2);
+        cache.put(cliente1.getId(), cliente1);
+        cache.put(cliente2.getId(), cliente2);
 
-        assertAll(
-                "Verificar que la caché contiene elementos antes de limpiar",
-                () -> assertEquals(2, cache.size(), "La caché debería contener dos elementos"),
-                () -> assertTrue(cache.containsKey(tarjeta1.getId()), "La caché debería contener tarjeta1"),
-                () -> assertTrue(cache.containsKey(tarjeta2.getId()), "La caché debería contener tarjeta2")
-        );
+        assertAll( () -> {
+            assertEquals(2, cache.size(), "La caché debería contener dos clientes");
+            assertTrue(cache.containsKey(cliente1.getId()), "La caché debería contener cliente1");
+            assertTrue(cache.containsKey(cliente2.getId()), "La caché debería contener cliente2");
+        });
 
         cache.clear();
 
-        assertAll(
-                "Verificar que la caché se vacía correctamente",
-                () -> assertEquals(0, cache.size(), "La caché debería estar vacía"),
-                () -> assertFalse(cache.containsKey(tarjeta1.getId()), "La caché no debería contener tarjeta1"),
-                () -> assertFalse(cache.containsKey(tarjeta2.getId()), "La caché no debería contener tarjeta2")
-        );
+        assertAll( () -> {
+            assertEquals(0, cache.size(), "La caché debería estar vacía");
+            assertFalse(cache.containsKey(cliente1.getId()), "La caché no debería contener cliente1");
+            assertFalse(cache.containsKey(cliente2.getId()), "La caché no debería contener cliente2");
+        });
     }
 
     @Test
     void size() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(3);
-        Tarjeta tarjeta1 = Tarjeta1();
-        Tarjeta tarjeta2 = Tarjeta2();
-        Tarjeta tarjeta3 = Tarjeta3();
+        CacheImpl cache = new CacheImpl(3);
+        Cliente cliente1 = Cliente1();
+        Cliente cliente2 = Cliente2();
+        Cliente cliente3 = Cliente.builder().id(3L).build();
 
         assertEquals(0, cache.size(), "La caché debería estar vacía al inicio");
 
-        cache.put(tarjeta1.getId(), tarjeta1);
-        assertEquals(1, cache.size(), "La caché debería contener un elemento");
+        cache.put(cliente1.getId(), cliente1);
+        assertEquals(1, cache.size(), "La caché debería contener un cliente");
 
-        cache.put(tarjeta2.getId(), tarjeta2);
-        assertEquals(2, cache.size(), "La caché debería contener dos elementos");
+        cache.put(cliente2.getId(), cliente2);
+        assertEquals(2, cache.size(), "La caché debería contener dos clientes");
 
-        cache.put(tarjeta3.getId(), tarjeta3);
-        assertEquals(3, cache.size(), "La caché debería contener tres elementos");
+        cache.put(cliente3.getId(), cliente3);
+        assertEquals(3, cache.size(), "La caché debería contener tres clientes");
 
         cache.clear();
         assertEquals(0, cache.size(), "La caché debería estar vacía después de limpiar");
-    }
-
-
-    @Test
-    void values() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(3);
-        Tarjeta tarjeta1 = Tarjeta1();
-        Tarjeta tarjeta2 = Tarjeta2();
-        Tarjeta tarjeta3 = Tarjeta3();
-
-        cache.put(tarjeta1.getId(), tarjeta1);
-        cache.put(tarjeta2.getId(), tarjeta2);
-        cache.put(tarjeta3.getId(), tarjeta3);
-
-        Collection<Tarjeta> valores = cache.values();
-        Collection<Tarjeta> finalValores = valores;
-        assertAll(
-                "Verificar que los valores devueltos son correctos",
-                () -> assertEquals(3, finalValores.size(), "La colección de valores debería contener tres elementos"),
-                () -> assertTrue(finalValores.contains(tarjeta1), "La colección de valores debería contener tarjeta1"),
-                () -> assertTrue(finalValores.contains(tarjeta2), "La colección de valores debería contener tarjeta2"),
-                () -> assertTrue(finalValores.contains(tarjeta3), "La colección de valores debería contener tarjeta3")
-        );
-
-        cache.clear();
-        valores = cache.values();
-        assertEquals(0, valores.size(), "La colección de valores debería estar vacía después de limpiar");
-    }
-
-
-    @Test
-    void isEmpty() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(3);
-
-        assertTrue(cache.isEmpty(), "La caché debería estar vacía al inicio");
-    }
-
-
-    @Test
-    void isNotEmpty() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(3);
-        Tarjeta tarjeta1 = Tarjeta1();
-
-        cache.put(tarjeta1.getId(), tarjeta1);
-
-        assertTrue(cache.isNotEmpty(), "La caché debería contener elementos y no estar vacía");
-    }
-
-
-    @Test
-    void isEmptyFalse() {
-        CacheImpl<Long, Tarjeta> cache = new CacheImpl<>(3);
-        Tarjeta tarjeta1 = Tarjeta1();
-
-        cache.put(tarjeta1.getId(), tarjeta1);
-
-        assertFalse(cache.isEmpty(), "La caché no debería estar vacía después de añadir un elemento");
     }
 }
