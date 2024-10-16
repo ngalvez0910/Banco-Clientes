005572689a6e426b27414fa31d74a6a40a0638b1
Mario de Domingo Alvarez <wolverine.mda.307@gmail.com>
2024-10-16 16:08:37 +0200

    added javaDoc to repositories

--- Banco_Clientes_Grupal/src/main/java/org/example/clientes/repositories/ClienteRepository.java
+++ Banco_Clientes_Grupal/src/main/java/org/example/clientes/repositories/ClienteRepository.java
@@ -6,16 +6,58 @@ import reactor.core.publisher.Flux;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interfaz que define las operaciones CRUD para el manejo de clientes.
+ *
+ * @author Jaime León, Natalia González, German Fernandez, Alba García, Mario de Domingo
+ * @version 1.0-SNAPSHOT
+ */
 public interface ClienteRepository {
+
+    /**
+     * Obtiene una lista de todos los clientes.
+     *
+     * @return Lista de {@link Cliente} que representan todos los clientes en la base de datos.
+     */
     List<Cliente> getAll();
 
+    /**
+     * Obtiene un cliente por su ID.
+     *
+     * @param id ID del cliente que se desea obtener.
+     * @return Un objeto {@link Optional} que contiene el cliente si se encuentra, o vacío si no.
+     */
     Optional<Cliente> getById(long id);
 
+    /**
+     * Crea un nuevo cliente en la base de datos.
+     *
+     * @param cliente El cliente a crear.
+     * @return El cliente creado con su ID generado.
+     */
     Cliente create(Cliente cliente);
 
+    /**
+     * Actualiza un cliente existente en la base de datos.
+     *
+     * @param id ID del cliente a actualizar.
+     * @param cliente El objeto {@link Cliente} con los nuevos datos.
+     * @return El cliente actualizado.
+     */
     Cliente update(long id, Cliente cliente);
 
+    /**
+     * Elimina un cliente de la base de datos.
+     *
+     * @param id ID del cliente a eliminar.
+     * @return {@code true} si se eliminó con éxito, {@code false} si no se encontró el cliente.
+     */
     boolean delete(long id);
 
+    /**
+     * Elimina todos los clientes de la base de datos.
+     *
+     * @return {@code true} si se eliminaron clientes, {@code false} si no había clientes para eliminar.
+     */
     boolean deleteAll();
 }
--- Banco_Clientes_Grupal/src/main/java/org/example/clientes/repositories/TarjetaRemoteRepository.java
+++ Banco_Clientes_Grupal/src/main/java/org/example/clientes/repositories/TarjetaRemoteRepository.java
@@ -3,8 +3,13 @@ package org.example.clientes.repositories;
 import org.example.clientes.model.Tarjeta;
 import org.example.common.Repository;
 
-import java.util.List;
-
+/**
+ * Interfaz para el repositorio remoto de tarjetas, que extiende la interfaz general de repositorios.
+ * Permite la gestión de entidades de tipo Tarjeta.
+ *
+ * @author Jaime León, Natalia González, German Fernandez, Alba García, Mario de Domingo
+ * @version 1.0-SNAPSHOT
+ */
 public interface TarjetaRemoteRepository extends Repository<Tarjeta, Long> {
 
 }
