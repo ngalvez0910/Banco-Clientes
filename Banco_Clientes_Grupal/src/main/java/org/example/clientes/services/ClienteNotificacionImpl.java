bf52361a6b0f7ec2194a5f6368bc185ab8e70328
Mario de Domingo Alvarez <wolverine.mda.307@gmail.com>
2024-10-16 16:32:44 +0200

    added javaDoc to services

--- Banco_Clientes_Grupal/src/main/java/org/example/clientes/services/ClienteNotificacionImpl.java
+++ Banco_Clientes_Grupal/src/main/java/org/example/clientes/services/ClienteNotificacionImpl.java
@@ -5,16 +5,31 @@ import org.example.clientes.model.Notificacion;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
+/**
+ * Implementación de la interfaz {@link ClienteNotificacion} para gestionar notificaciones de clientes.
+ * Esta clase sigue el patrón Singleton y utiliza un flujo reactivo para manejar las notificaciones.
+ *
+ * @author Jaime León, Natalia González, German Fernandez, Alba García, Mario de Domingo
+ * @version 1.0-SNAPSHOT
+ */
 public class ClienteNotificacionImpl implements ClienteNotificacion {
     private static ClienteNotificacionImpl INSTANCE = new ClienteNotificacionImpl();
 
     private final Flux<Notificacion<Cliente>> clientesNotificationFlux;
     private FluxSink<Notificacion<Cliente>> clientesNotification;
 
+    /**
+     * Constructor privado que inicializa el flujo de notificaciones de clientes.
+     */
     public ClienteNotificacionImpl() {
         this.clientesNotificationFlux = Flux.<Notificacion<Cliente>>create(emitter -> this.clientesNotification = emitter).share();
     }
 
+    /**
+     * Obtiene la instancia única de {@link ClienteNotificacionImpl}.
+     *
+     * @return la instancia única de ClienteNotificacionImpl
+     */
     public static ClienteNotificacionImpl getInstance() {
         if (INSTANCE == null) {
             INSTANCE = new ClienteNotificacionImpl();
@@ -22,11 +37,17 @@ public class ClienteNotificacionImpl implements ClienteNotificacion {
         return INSTANCE;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Flux<Notificacion<Cliente>> getNotificationAsFlux() {
         return clientesNotificationFlux;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void notify(Notificacion<Cliente> notificacion) {
         clientesNotification.next(notificacion);
